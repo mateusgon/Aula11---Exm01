011a4186778cd77582d05015753a0c717a76f0cc
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-10-05 22:33:55 -0300

    Adicionando tela

---
+++ src/br/ufjf/dcc171/Aula11Arquivos01.java
@@ -0,0 +1,12 @@
+
+package br.ufjf.dcc171;
+
+public class Aula11Arquivos01 {
+
+    public static void main(String[] args) {
+        JanelaPasta janela = new JanelaPasta();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
